e2edb9a921fb88d37faf81a6de2f759aff430071
sashivulli <127401182+sashivulli@users.noreply.github.com>
2023-05-17 12:15:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,4 @@ streamlit.dataframe(my_fruit_list)
 
 #Lets put a pic list here so they can pick the fruit they want to include
 
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocado","Strawberries"])
